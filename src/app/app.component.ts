afccd18662d457ae8c7bbae5db20e3eb4ff00065
nartc <nartc7789@gmail.com>
2024-08-27 16:59:19 -0500

    add ngt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { NgtCanvas } from 'angular-three';
+import { Experience } from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ``,
+  template: `
+    <ngt-canvas [sceneGraph]="sceneGraph" />
+  `,
   host: { class: 'block h-dvh w-full' },
+  imports: [NgtCanvas],
 })
-export class AppComponent {}
+export class AppComponent {
+  sceneGraph = Experience;
+}
